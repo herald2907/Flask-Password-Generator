820d13b04dcc6668e23468d604b827fc9099338c
herald2907 <heraldjay29@gmail.com>
2023-01-23 23:31:09 +0800

    add method and password json

--- python-flask-backend/app.py
+++ python-flask-backend/app.py
@@ -1,6 +1,27 @@
-from flask import Flask
+from flask import Flask,jsonify,request,json
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
     return "Hello World, from Flask!"
+
+
+@app.route("/api/password", methods = ['GET', 'POST'])
+def passwordMethods():
+    if request.method == 'GET':
+    #Call the method to Get all of the puppies
+        return "You cannot access here!"
+
+    elif request.method == 'POST':
+    #Call the method to make a new puppy
+        return postPassword()
+
+
+def postPassword():
+    #Password Generator start here
+    requestData = request.form
+    with open('password.json') as f:
+        data = json.load(f)
+
+
+    return data
